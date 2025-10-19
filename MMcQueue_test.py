db42d92dc47c594c7fd09367c770a3c9a92e7e56
meganou <ou27@up.edu>
2025-10-19 11:14:44 -0700

    MO - Adding tests

---
+++ MMcQueue_test.py
@@ -0,0 +1,18 @@
+from unittest import TestCase
+
+
+class TestMMcQueue(TestCase):
+    def test_c(self):
+        self.fail()
+
+    def test_ro(self):
+        self.fail()
+
+    def test_is_valid(self):
+        self.fail()
+
+    def test_is_feasible(self):
+        self.fail()
+
+    def test__calc_metrics(self):
+        self.fail()
