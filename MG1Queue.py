c62dab5b0105db0b2906811f6880053808b866cd
toby <toby.okoji@gmail.com>
2025-10-28 00:44:00 -0700

    MG1 revision. Lines 55-67 needed or else it fails 2 tests.

--- MG1Queue.py
+++ MG1Queue.py
@@ -7,16 +7,15 @@ class MG1Queue(BaseQueue.BaseQueue):
     MG1 queue is singler server with
     Poisson arrivals (regardless of service time distribution type)
     """
-    #TODO: Hi could you please add header comments for each method?
     def __init__(self, lamda, mu, sigma = 0.0):
-        #TODO: Toby, you shoudl delete line 15 as it defeats the purpose of Object Oriented,
-        # since you defined sigma as a property, you do not need to define it
-        # in the constructor (Megan)
-        #self._sigma = math.nan  #service-time std dev
         super().__init__(lamda, mu)
         self.sigma = sigma  #validate via setter
 
     def __str__(self):
+        """
+        Method that prints a string representation of a queue's object state.
+        Returns: None
+        """
         return (
             f"MG1Queue instance at {id(self)}"
             f"\n\t lamda: {self.lamda}"
@@ -33,6 +32,10 @@ class MG1Queue(BaseQueue.BaseQueue):
     #service time variance property
     @property
     def sigma(self):
+        """
+        Getter method for sigma property
+        Returns: service tive variance
+        """
         return self._sigma
 
     @sigma.setter
@@ -47,25 +50,20 @@ class MG1Queue(BaseQueue.BaseQueue):
     def is_valid(self) -> bool:
         if not super().is_valid():  #checks lamda and mu
             return False
-        #TODO: toby, please use the getter for sigma and not reference the actual
-        # variable. self.sigma should work just fine
-        return not math.isnan(self._sigma)
+        return not math.isnan(self.sigma)
 
     def is_feasible(self) -> bool:
         """
-        #TODO: please use docstrings format; also i do not believe you need this method because it does not introduce anything new
-        Feasibility for MG1 depends only on lamda and mu (p < 1).
-        Don't make feasibility check depend on sigma
+        Checks whether the MG1 queue system is feasible.
+        Returns: True if lamda / mu < 1. False otherwise.
         """
         #lamda, mu must be valid numbers
-        #TODO: I notice that you are referencing a lot of properties directly instead of using the getter methods.
-        # please try getting into the habit of using the getters that Mitchell/OOP requires from us
-        if math.isnan(self._lamda) or math.isnan(self._mu):
+        if math.isnan(self.lamda) or math.isnan(self.mu):
             return False
 
-        if self._mu == 0:
+        if self.mu <= 0:
             return False
-        rho = self._lamda / self._mu
+        rho = self.lamda / self.mu
         return rho < 1
 
     def _calc_metrics(self):
@@ -84,13 +82,8 @@ class MG1Queue(BaseQueue.BaseQueue):
 
         rho = self.ro
         denominator = 2 * (1 - rho)
-        #TODO: not sure if we need this if statement because if rho is feasible, then
-        # the denominator will never be 0, so you can go straight to the calculation
-        if denominator <= 0 or math.isnan(denominator):
-            self._lq = math.nan
-        else:
-            #TODO: good job using the getter methods here!
-            self._lq = (rho ** 2 + (self.lamda ** 2) * (self.sigma ** 2)) / denominator
+
+        self._lq = (rho ** 2 + (self.lamda ** 2) * (self.sigma ** 2)) / denominator
 
         self._p0 = 1 - rho
         self._recalc_needed = False
